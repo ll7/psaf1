5fe8b785dcb6ee19cdca11820d28901ada87f949
Jonas W <jonas.wilfert@student.uni-augsburg.de>
2020-11-20 12:21:16 +0100

    drive car with full speed

--- psaf_ros/psaf_steering/src/hello.py
+++ psaf_ros/psaf_steering/src/hello.py
@@ -6,12 +6,19 @@ from carla_msgs.msg import CarlaEgoVehicleControl
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub_carla = rospy.Publisher('/carla/ego_vehicle/vehicle_control_cmd', CarlaEgoVehicleControl, queue_size=10)
+
     rospy.init_node('hello', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
+    contr = CarlaEgoVehicleControl()
+    contr.throttle = 1.0
+
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
+        pub_carla.publish(contr)
         rate.sleep()
 
 if __name__ == '__main__':
