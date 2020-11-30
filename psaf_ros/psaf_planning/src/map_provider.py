8ac71e4e11c00fb883d2110dedb00fcd341a4717
Sebastian Fischer <sebastian.fischer@embeddedlabs.de>
2020-11-30 13:51:23 +0100

    Update error handling, update status messages

--- psaf_ros/psaf_planning/src/map_provider.py
+++ psaf_ros/psaf_planning/src/map_provider.py
@@ -22,31 +22,39 @@ class MapProvider:
 
     def __init__(self, init_rospy: bool = False):
         self.osm_file = tempfile.NamedTemporaryFile(suffix=".osm")
+        # opendrive map received from the _world_info_subscriber
         self.map: str = None
-        self.map_ready: bool = False
+        # name of the map received from the _world_info_subscriber
         self.map_name: str = None
+        # boolean that represents the availability of the opendrive map -> the if the map was received by the subscriber
+        self.map_ready: bool = False
+
+        # starts the logging node , normally only needed if the module is used independently
         if init_rospy:
             rospy.init_node('mapProvider', anonymous=True)
 
+        # Subscriber to receive the currently loaded map
         self._world_info_subscriber = rospy.Subscriber(
             "/carla/world_info", CarlaWorldInfo, self.update_world)
 
     def __del__(self):
+        # closing the TemporaryFile deletes it
         self.osm_file.close()
+        rospy.loginfo("MapProvider: Cleanup done")
 
     def update_world(self, world_info: CarlaWorldInfo):
         """
         Check if a new map was sent and receive it
         """
         self.map_ready = False
-        rospy.loginfo("Received new map info")
+        rospy.loginfo("MapProvider: Received new map info")
         if self.map_name == world_info.map_name:
-            rospy.loginfo("Map already loaded")
+            rospy.loginfo("MapProvider: Map already loaded")
         else:
             self.map_name = world_info.map_name
             self.map = world_info.opendrive
             self.map_ready = True
-            rospy.loginfo("Received: " + self.map_name)
+            rospy.loginfo("MapProvider: Received: " + self.map_name)
 
     def convert_to_osm(self) -> str:
         """
@@ -55,11 +63,19 @@ class MapProvider:
         lanelet = self.convert_od_to_lanelet()
         if lanelet is not None:
             l2osm = L2OSMConverter(
-                "+proj=omerc +lat_0=49 +lonc=8 +alpha=0 +k=1 +x_0=0 +y_0=0 +gamma=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0")
+                "+proj=omerc +lat_0=0 +lonc=0 +alpha=0 +k=1 +x_0=0 +y_0=0 +gamma=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0")
             openstreetmap = etree.tostring(l2osm(lanelet), xml_declaration=True, encoding="UTF-8", pretty_print=True)
             self.osm_file.write(openstreetmap)
+            rospy.loginfo("MapProvider: Temporary file created:" + os.path.abspath(self.osm_file.name))
+            return os.path.abspath(self.osm_file.name)
+        else:
+            rospy.logerr("MapProvider: lanelet not available")
+            rospy.logerr("MapProvider: Couldn't create temporary file")
+            return ""
 
-        return os.path.abspath(self.osm_file.name)
+    """
+    Currently not in use since CommonRoad lanlets are not compatible with lanelet2
+    """
 
     def convert_od_to_lanelet(self) -> Scenario:
         """
@@ -67,14 +83,17 @@ class MapProvider:
         """
         lanelet: Scenario = None
         if self.map_ready:
-            rospy.loginfo("Start conversion...")
+            rospy.loginfo("MapProvider: Start conversion...")
             opendrive = parse_opendrive(etree.parse(BytesIO(self.map.encode('utf-8'))).getroot())
             roadNetwork = Network()
             roadNetwork.load_opendrive(opendrive)
             lanelet = roadNetwork.export_commonroad_scenario()
-            rospy.loginfo("Conversion done!")
+            rospy.loginfo("MapProvider: Conversion done!")
         return lanelet
 
+    """
+    The generate methods are just for debugging purposes 
+    """
 
     def generate_osm_file(self):
         lanelet = self.convert_od_to_lanelet()
@@ -86,8 +105,10 @@ class MapProvider:
             # write osm file
             with open(self.map_name + ".osm", "wb") as file_out:
                 file_out.write(openstreetmap)
+            rospy.loginfo("MapProvider: Wrote file" + self.map_name + ".osm")
         else:
-            rospy.logerr("lanelet not available")
+            rospy.logerr("MapProvider: lanelet not available")
+            rospy.logerr("MapProvider: No file generated")
 
     def generate_com_road_file(self):
         lanelet = self.convert_od_to_lanelet()
@@ -102,12 +123,16 @@ class MapProvider:
             )
             # write CommonRoad data to xml file
             writer.write_to_file(self.map_name + ".xml", OverwriteExistingFile.ALWAYS)
+            rospy.loginfo("MapProvider: Wrote file" + self.map_name + ".xml")
         else:
-            rospy.logerr("lanelet not available")
+            rospy.logerr("MapProvider: lanelet not available")
+            rospy.logerr("MapProvider: No file generated")
 
 
 def main():
     provider = MapProvider(True)
+    if not provider.convert_to_osm():
+        rospy.loginfo("Check")
     while not provider.map_ready:
         rospy.loginfo("Waiting")
     provider.generate_osm_file()
