cb847db13e119c4632afb7bcbb872e03cf811eed
Markus <64690473+mkep-dev@users.noreply.github.com>
2020-11-30 15:36:33 +0100

    Add abstraction layer for car with simple gps and ackermann control (#11)
    * Add abstraction layer for car with simple gps and ackermann control
    
    * Improve interface and add vehicle status for orientation
    
    Co-authored-by: Markus Keppeler <markus.keppeler@uni-a.de>

---
+++ psaf_ros/psaf_abstraction_layer/src/psaf_abstraction_layer/VehicleStatus.py
@@ -0,0 +1,64 @@
+import rospy
+from carla_msgs.msg import CarlaEgoVehicleStatus
+from geometry_msgs.msg import Quaternion,Accel
+from tf.transformations import euler_from_quaternion
+
+class VehicleStatus:
+    """
+    Vehicle Status
+    Attr:
+        orientation     the orientation
+        acceleration    the acceleration as geometry_msgs.msg.Accel
+        velocity        the velocity in m/s
+    """
+
+
+    def __init__(self, orientation: Quaternion = Quaternion(), acceleration: Accel = Accel(), velocity: float = 0.):
+
+        self.orientation = orientation
+        self.acceleration = acceleration
+        self.velocity = velocity
+
+    def __str__(self) -> str:
+        return "orientation:'{}'-acc:'{}'-vel:'{}m/s'".format(self.orientation, self.acceleration, self.velocity)
+
+    def get_orientation_as_euler(self):
+        """
+        Returns the quanterian as roll pitch yaw
+        :return: [0]= roll, [1] = pitch, [2] = yaw
+        """
+        return euler_from_quaternion((self.orientation.x,self.orientation.y,self.orientation.z,self.orientation.w))
+
+
+class VehicleStatusProvider:
+
+    def __init__(self, role_name: str):
+        """
+        Constructor
+        :param role_name: the role name of the vehicle
+        """
+
+        self.status = VehicleStatus()
+        self.__subscriber = rospy.Subscriber("/carla/{}/vehicle_status".format(role_name), CarlaEgoVehicleStatus,
+                                             self.__update_status)
+        self.__listener = None
+
+    def __update_status(self,message:CarlaEgoVehicleStatus):
+        self.status = VehicleStatus(message.orientation,message.acceleration,message.velocity)
+        if self.__listener != None:
+            self.__listener(self.status)
+
+    def set_on_status_update_listener(self, func):
+        """
+        Set function to be called with the new VehicleStatus as parameter
+        :param func: the function
+        :return: None
+        """
+        self.__listener = func;
+
+    def get_status(self) -> VehicleStatus:
+        """
+        Return the current status
+        :return: the current status
+        """
+        return self.status
