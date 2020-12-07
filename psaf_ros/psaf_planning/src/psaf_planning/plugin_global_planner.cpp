65fed9f5d4860896fc7ae827a7eb1d4dd8fc21bd
Jonas <bloodworkxgaming@gmail.com>
2020-12-07 22:04:34 +0100

    F-16: Integrate global plugin with move_base (#21)
    * Add abstraction layer for car with simple gps and ackermann control (#11)
    
    * Add abstraction layer for car with simple gps and ackermann control
    
    * Improve interface and add vehicle status for orientation
    
    Co-authored-by: Markus Keppeler <markus.keppeler@uni-a.de>
    
    * F 6 map access (#12)
    
    Add MapProvider, add vehicle
    
    * F 07 pathplanning (#13)
    
    * Implement a path_provider
    
    Class PathProvider computes a feasible path from a starting point A to a target Point B by computing the nearest lanelets and performing a dijkstra graph search on a provided .osm map.
    
    * Add import of MapProvider and GPS_Position, according to the project structure
    
    * Adjust project structure and imports
    
    * Fix ros python package setup
    
    * cpp setup working
    
    * Add plugin
    
    * Add goal panel
    
    * Add load path from file
    Clean up
    
    * Add automatic file detection
    
    * Use bag instead of binary file
    
    * Add xml files
    
    * add launch file
    
    * inegration path_provider
    
    * Prune the path of its unneeded elements
    Added new test condition in main
    
    * Introduce Path and PoseStamped with calculation of Quaternions
    
    * Create path message and provide possibility to display the path of a test example in main()
    
    * Fix in path calculation
    Orientation based on global frame? yet to be fixed
    
    * Add debug possibility by generating intermediate results
    
    * Add Subscriber for goal input and current GPS Position
    
    * Serialize path message and create temporary file
    Add Subscriber for goal input and current GPS Position
    
    Furthermore Refactoring and structural changes to the internal structure
    
    * Use main() if launched not test()
    
    * Replace temporary files with bag files
    
    * BugFix target_pose not actually pose but PoseStamped
    
    * Fix: catch empty route and Add additional logging
    
    * Added frame_id to header of each PoseStamped in path
    
    Co-authored-by: t.ciemala@outlook.de <pro3e2n1te!>
    
    * BugFix imports in map_provider
    
    * Remove client.wait because no result is send?
    
    Do not wait for a result because no result is send.
    Dynamic replanning is possible now.
    
    Co-authored-by: Markus <64690473+mkep-dev@users.noreply.github.com>
    Co-authored-by: Markus Keppeler <markus.keppeler@uni-a.de>
    Co-authored-by: basti128 <62876320+basti128@users.noreply.github.com>
    Co-authored-by: TobiasCie <t.ciemala@outlook.de>
    Co-authored-by: t.ciemala@outlook.de <pro3e2n1te!>
    Co-authored-by: Sebastian Fischer <sebastian.fischer@embeddedlabs.de>

---
+++ psaf_ros/psaf_planning/src/psaf_planning/plugin_global_planner.cpp
@@ -0,0 +1,83 @@
+#include <pluginlib/class_list_macros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include "plugin_global_planner.h"
+
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+//register this planner as a BaseGlobalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(psaf_global_planner::GlobalPlanner, nav_core::BaseGlobalPlanner)
+
+namespace psaf_global_planner {
+
+    GlobalPlanner::GlobalPlanner(): costmap_ros(NULL) {
+
+    }
+
+    GlobalPlanner::GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros) {
+        initialize(name, costmap_ros);
+    }
+
+    void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+        if(!init){
+            this->costmap_ros = costmap_ros; //initialize the costmap_ros_ attribute to the parameter.
+            ROS_INFO("This planner is initialized");
+            init = true;
+        }
+        else
+            ROS_WARN("This planner has already been initialized... doing nothing");
+    }
+
+    bool GlobalPlanner::loadPath(const std::string& filename) {
+        // Read from File to pathMsg
+        ROS_INFO("Loading path from: %s", filename.c_str());
+        nav_msgs::Path pathMsg;
+        rosbag::Bag bag;
+        bag.open(filename, rosbag::bagmode::Read);
+
+        std::vector<std::string> topics;
+        topics.push_back(std::string("Path"));
+
+        rosbag::View view(bag, rosbag::TopicQuery(topics));
+        foreach(rosbag::MessageInstance const m, view)
+        {
+            nav_msgs::PathPtr s = m.instantiate<nav_msgs::Path>();
+            if (s != NULL)
+                path = s->poses;
+        }
+        bag.close();
+    }
+
+    bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan) {
+        if(init && ros::ok()){
+            std::string filename;
+            for (const auto & entry : std::filesystem::directory_iterator("/tmp"))
+                if(entry.path().string().find(".path") != std::string::npos) {
+                    filename = entry.path().string();
+                    break;
+                }
+            loadPath(filename); //get path from file
+            if(path.size() > 0) {
+                if(true) {
+                    plan = path; //move the path to the plan vector witch is used by the move_base to follow the path
+                    ROS_INFO("Path size: %d vs %d(msg)", plan.size(), path.size());
+                    ROS_ERROR("Global planner was successful");
+                    return true;
+                } else {
+                    plan.push_back(start);
+                    ROS_ERROR("Path start and end positions don't match");
+                    return false;
+                }
+            } else {
+                ROS_ERROR("No path received, execute the path pLaner package and try again");
+                plan.push_back(start);
+                return false;
+            }
+        } else {
+            ROS_ERROR("Planner not initialized");
+            plan.push_back(start);
+            return false;
+        }
+    }
+};
---
+++ psaf_ros/psaf_planning/src/psaf_planning/plugin_global_planner.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Path.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+#include <nav_msgs/Path.h>
+#include <string>
+#include <filesystem>
+
+namespace psaf_global_planner {
+    class GlobalPlanner : public nav_core::BaseGlobalPlanner {
+    public:
+        GlobalPlanner();
+        GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+        /** overridden classes from interface nav_core::BaseGlobalPlanner **/
+        void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+        bool makePlan(const geometry_msgs::PoseStamped& start,
+                      const geometry_msgs::PoseStamped& goal,
+                      std::vector<geometry_msgs::PoseStamped>& plan
+        );
+    private:
+        bool loadPath(const std::string& filename);
+        std::vector<geometry_msgs::PoseStamped> path;
+        costmap_2d::Costmap2DROS* costmap_ros;
+        bool init = false;
+    };
+};
