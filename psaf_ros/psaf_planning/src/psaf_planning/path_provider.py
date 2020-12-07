36f7f20dea8c1b8919243ee74b24b59cd8969f92
TobiasCie <t.ciemala@outlook.de>
2020-12-07 16:11:49 +0100

    inegration path_provider
    * Prune the path of its unneeded elements
    Added new test condition in main
    
    * Introduce Path and PoseStamped with calculation of Quaternions
    
    * Create path message and provide possibility to display the path of a test example in main()
    
    * Fix in path calculation
    Orientation based on global frame? yet to be fixed
    
    * Add debug possibility by generating intermediate results
    
    * Add Subscriber for goal input and current GPS Position
    
    * Serialize path message and create temporary file
    Add Subscriber for goal input and current GPS Position
    
    Furthermore Refactoring and structural changes to the internal structure
    
    * Use main() if launched not test()
    
    * Replace temporary files with bag files
    
    * BugFix target_pose not actually pose but PoseStamped
    
    * Fix: catch empty route and Add additional logging
    
    * Added frame_id to header of each PoseStamped in path
    
    Co-authored-by: t.ciemala@outlook.de <pro3e2n1te!>

--- psaf_ros/psaf_planning/src/psaf_planning/path_provider.py
+++ psaf_ros/psaf_planning/src/psaf_planning/path_provider.py
@@ -7,12 +7,21 @@ from lanelet2.core import GPSPoint
 from lanelet2.io import Origin
 import rospy
 from psaf_planning.map_provider import MapProvider
-from psaf_abstraction_layer.GPS import GPS_Position
+from psaf_abstraction_layer.GPS import GPS_Position, GPS_Sensor
+from nav_msgs.msg import Path
+from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import PoseStamped, Point, Quaternion
+from tf.transformations import quaternion_from_euler
+import math
+import os
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import rosbag
 
 
 class PathProvider:
 
-    def __init__(self, init_rospy: bool = False, polling_rate: int = 1, timeout_iter: int = 10):
+    def __init__(self, init_rospy: bool = False, polling_rate: int = 1, timeout_iter: int = 10, role_name: str = "ego_vehicle"):
         """
         Class PathProvider provides a feasible path from a starting point A to a target Point B by computing
         the nearest lanelets and performing a dijkstra graph search on a provided .osm map.
@@ -23,7 +32,9 @@ class PathProvider:
         if init_rospy:
             # initialize node
             rospy.init_node('pathProvider', anonymous=True)
-
+        self.role_name = role_name
+        self.GPS_Sensor = GPS_Sensor(role_name=self.role_name)
+        rospy.Subscriber("/psaf/goal/set", NavSatFix, self._callback_goal)
         self.origin = Origin(0, 0)
         self.projector = UtmProjector(self.origin)
         self.map_provider = MapProvider()
@@ -31,29 +42,61 @@ class PathProvider:
         if not self.map_path:
             rospy.logerr("PathProvider: No Map received!")
             self.lanelet_map = None
-            self.path = None
         else:
             self.lanelet_map = lanelet2.io.load(self.map_path, self.projector)
-            self.path = None
+        self.path = Path()
+        self.path_long = Path()
+        self.start = None
+        self.goal = None
 
-    def get_path_from_a_to_b(self, from_a: GPS_Position, to_b: GPS_Position):
+    def __del__(self):
+        # delete path bag files
+        item_list = os.listdir("/tmp")
+        for item in item_list:
+            if item.endswith(".path"):
+                os.remove(os.path.join("/tmp", item))
+
+    def _create_bag_file(self, filename: str):
         """
-        Returns the shortest path from a to be
-        :param  from_a: Start point [GPS Coord]
-        :param  to_b:   End point [GPS Coord]
-        :return: Path or None if no path was found at all, or no map information is received
+        Creates a bag file object
+        :param filename: filename of bag file
+        :return: bag file object
         """
+        # suffix set to be .path
+        suffix = ".path"
+        # directory
+        dir = "/tmp/"
+        return rosbag.Bag(str(dir + filename + suffix), "w")
+
+    def get_path_from_a_to_b(self, from_a: GPS_Position = None, to_b: GPS_Position = None, debug=False, long: bool = False):
+        """
+        Returns the shortest path from start to goal
+        :param  from_a: Start point [GPS Coord] optional
+        :param  to_b:   End point [GPS Coord] optional
+        :param debug: Default value = False ; Generate Debug output
+        :param long: boolean weather the long path should be returned
+        :return: Pruned or long Path or None if no path was found at all, or no map information is received
+        """
+        start = self.start
+        goal = self.goal
+
         if not self.lanelet_map:
             return self.path  # which is None, because no map was received
-        self._compute_route(from_a, to_b)
-        return self.path
 
-    def get_path(self):
-        """
-        Returns the current path
-        :return: Path or None
-        """
-        return self.path
+        if from_a is not None and to_b is not None:
+            start = from_a
+            goal = to_b
+
+        if not debug:
+            self._compute_route(start, goal)
+        else:
+            self._compute_route(start, goal, debug=True)
+
+        if long:
+            return self.path_long
+        else:
+            # return pruned path
+            return self.path
 
     def _get_map_path(self, polling_rate: int, timeout_iter: int):
         """
@@ -73,19 +116,66 @@ class PathProvider:
 
         return map_path
 
-    def _compute_route(self, from_a: GPS_Position, to_b: GPS_Position):
+    def _load_costum_map_path(self, path, origin):
+        """
+        Only used for testing puposes
+        :return: Updates the map
+        :param path: path to the map
+        :param origin: origin of the provided map
+        """
+        self.lanelet_map = lanelet2.io.load(path, UtmProjector(origin))
+
+    def _euclidean_2d_distance_from_to_position(self, objA: PoseStamped, objB: lanelet2.core.BasicPoint2d):
+        """
+        This helper function calculates the euclidean distance between 2 Points
+        :param objA: HAS! to be the path_long entry, thus a PoseStamped Object
+        :param objB: BasicPoint2D provided by the lanelet package
+        :return: The calculated distance
+        """
+        return ((objA.pose.position.x - objB.x) ** 2 + (objA.pose.position.y - objB.y) ** 2) ** 0.5
+
+    def _get_Pose_Stamped(self, pos: Point, prev_pos: Point):
+        """
+        This function calculates the euler angle alpha acoording to the direction vector of two given points.
+        Thereafter this information is transformed to the Quaternion representation
+         and the PoseStamped Object is generated
+        :param pos: Position in x,y,z
+        :param prev_pos: Previous Position in x,y,z
+        :return: PoseStamped
+        """
+        p = PoseStamped()
+
+        p.pose.position.x = pos.x
+        p.pose.position.y = pos.y
+        p.pose.position.z = pos.z
+
+        euler_angle_alpha = math.atan2(abs(pos.y - prev_pos.y), abs(pos.x - prev_pos.x))
+
+        # only 2D space is relevant, therefore angles beta and gamma can be set to zero
+        q = quaternion_from_euler(0.0, 0.0, euler_angle_alpha)
+        p.pose.orientation = Quaternion(*q)
+        p.header.frame_id = "map"
+
+        return p
+
+    def _compute_route(self, from_a: GPS_Position, to_b: GPS_Position, debug=False):
         """
         Compute shortest path
         :param from_a: Start point -- GPS Coord in float: latitude, longitude, altitude
         :param to_b: End point   -- GPS Coord in float: latitude, longitude, altitude
+        :param debug: Default value = False ; Generate Debug output
         """""
         rospy.loginfo("PathProvider: Computing feasible path from a to b")
 
         # generate traffic_rules based on participant type and location
         traffic_rules_ger = lanelet2.traffic_rules.create(lanelet2.traffic_rules.Locations.Germany,
                                                           lanelet2.traffic_rules.Participants.Vehicle)
+
         # generate routing_graph which represents the given map
         routing_graph = lanelet2.routing.RoutingGraph(self.lanelet_map, traffic_rules_ger)
+        if debug:
+            lanelet2.io.write("debuggraph.osm", routing_graph.getDebugLaneletMap(0))
+            rospy.loginfo("PathProvider: :DEBUG==TRUE: routing_graph written to 'debuggraph.osm'")
 
         # step1: get nearest lanelet to start point
         gps_point_start = GPSPoint(from_a.latitude, from_a.longitude, from_a.altitude)
@@ -101,31 +191,122 @@ class PathProvider:
         close_to_lanelets = lanelet2.geometry.findNearest(self.lanelet_map.laneletLayer, target_local_2d, 1)
         to_lanelet = close_to_lanelets[0][1]
 
-        # step3: compute shortest path
-        self.path = []
-        shortest_path = routing_graph.shortestPath(from_lanelet, to_lanelet)
-        for lane in shortest_path:
-            for pos in lane.centerline:
-                point = self._WayPoint(pos.x, pos.y, pos.z)
-                self.path.append(point)
+        # step3: compute shortest path, do not prune for path_long
+        path_long_list = []
+        route = routing_graph.getRoute(from_lanelet, to_lanelet, 0)
 
-        rospy.loginfo("PathProvider: Computation of feasible path done")
+        if debug:
+            laneletSubmap = route.laneletSubmap()
+            lanelet2.io.write("route.osm", laneletSubmap.laneletMap())
+            rospy.loginfo("PathProvider: :DEBUG==TRUE: route written to 'route.osm'")
 
-    class _WayPoint:
+        if route is not None:
+            shortest_path = route.shortestPath()
+            prev_point = None
 
-        def __init__(self, x, y, z):
-            self.x = x
-            self.y = -y
-            self.z = z
+            # long path
+            for lane in shortest_path:
+                for pos in lane.centerline:
+                    point = Point(pos.x, pos.y, pos.z)
+                    if prev_point is None:
+                        prev_point = point
+                    path_long_list.append(self._get_Pose_Stamped(point, prev_point))
+                    prev_point = point
+
+            # Path was found!
+            # Prune path
+            real_start_index = path_long_list.index(min(path_long_list, key=lambda
+                obj_list: self._euclidean_2d_distance_from_to_position(obj_list, start_local_2d)))
+            real_end_index = path_long_list.index(min(path_long_list, key=lambda
+                obj_list: self._euclidean_2d_distance_from_to_position(obj_list, target_local_2d)))
+
+            # create self.path messages
+            self.path.poses = path_long_list[real_start_index: real_end_index]
+            self.path.header.frame_id = "map"
+            self.path.header.seq = 1
+            self.path.header.stamp = rospy.Time.now()
+            # create self.path_long messages
+            self.path_long.poses = path_long_list
+            self.path_long.header.frame_id = "map"
+            self.path_long.header.seq = 1
+            self.path_long.header.stamp = rospy.Time.now()
+            rospy.loginfo("PathProvider: Computation of feasible path done")
+
+        else:
+            # no path was found
+            self.path = Path()
+            self.path_long = Path()
+            rospy.logerr("PathProvider: No possible path was found")
+
+
+    def _serialize_message(self, path: Path):
+        """
+        Serialize path message and write to bag file
+        """
+        bag_file = self._create_bag_file(filename="path")
+        bag_file.write('Path', path)
+        bag_file.close()
+        rospy.loginfo("PathProvider: Created bag file with path. /tmp/path.path")
+
+    def _callback_goal(self, data):
+        """
+        Callback function of psaf goal set subscriber
+        :param data: data received
+        """
+        self.goal = GPS_Position(latitude=data.latitude, longitude=data.longitude, altitude=data.altitude)
+        self.start = self.GPS_Sensor.get_position()
+        rospy.loginfo("PathProvider: Received start and goal position")
+        self._serialize_message(self.get_path_from_a_to_b())
+        self._trigger_move_base(self.path.poses[-1])
+        rospy.loginfo("PathProvider: global planner plugin triggered")
+
+    def _trigger_move_base(self, target: PoseStamped):
+        """
+        This function triggers the move_base by publishing the goal, which is later used for sanity checking
+        """
+        client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        client.wait_for_server()
+
+        goal = MoveBaseGoal()
+        goal.target_pose = target
+
+        client.send_goal(goal)
+        wait = client.wait_for_result()
+        if not wait:
+            rospy.logerr("PathProvider: Action server not available!")
+            rospy.signal_shutdown("PathProvider: Action server not available!")
+        else:
+            rospy.loginfo(client.get_result())
+
+
+def test():
+    """
+    Provide a simple standalone test scenario
+    """
+    provider = PathProvider(init_rospy=True)
+    #provider._load_costum_map_path("src/psaf_planning/map.osm", Origin(49, 8))
+    start = GPS_Position(0.000334, 0.000701, 0)  # in x,y,z: 78,1/-38,5
+    end = GPS_Position(-0.000042, 0.000818, 0)  # in x,y,z: 90/4,7
+    path = provider.get_path_from_a_to_b(start, end, debug=True)
+    # get long path anyway
+    long_path = provider.path_long
+    provider._serialize_message(path)
+    print("Main finished")
+
+    pub = rospy.Publisher("/path", Path, queue_size=10)
+    r = rospy.Rate(1)  # 1 Hz - once a second
+    while not rospy.is_shutdown():
+        pub.publish(path)
+        r.sleep()
+
+
+def main():
+    provider = PathProvider(init_rospy=True)
+    rospy.spin()
 
 
 if __name__ == "__main__":
     try:
-        provider = PathProvider(init_rospy=True)
-        start = GPS_Position(0.000719, -0.001346, 0)
-        end = GPS_Position(0.001844, 0.001209, 0)
-        path = provider.get_path_from_a_to_b(start, end)
-        print("Main finished")
-
+        main()
     except rospy.ROSInterruptException:
         pass
