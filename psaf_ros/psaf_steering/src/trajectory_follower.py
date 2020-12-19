7023dcf22a01905061ab135600bfb766d908de64
tintenjo <johannes.tintenherr.de>
2020-12-19 17:33:34 +0100

    Trajectory_Follower updated, dynamic speed control added

--- psaf_ros/psaf_steering/src/trajectory_follower.py
+++ psaf_ros/psaf_steering/src/trajectory_follower.py
@@ -9,8 +9,9 @@ import rospy
 from matplotlib import pyplot as plt
 
 from nav_msgs.msg import Path, Odometry
-from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, Twist, PointStamped
-from tf.transformations import euler_from_quaternion
+from std_msgs.msg import Header
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, PointStamped
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 plot = False
 
@@ -23,15 +24,15 @@ class TrajectoryFollower(object):
         self.control_cmd = AckermannDrive()
         self.target_reached = False
 
-        self.target_speed = 30.0
-        self.min_speed = 10.0
+        self.target_speed = 50.0
+        self.min_speed = 15.0
 
         self.current_location = None
         self.current_orientation = None
         self.current_speed = None
 
         self.min_distance = 3
-        self.buffer_size = 300
+        self.buffer_size = 100
         self.local_plan = deque(maxlen=self.buffer_size)
         self.global_plan = deque()
 
@@ -44,8 +45,10 @@ class TrajectoryFollower(object):
         self.odometry_subscriber = rospy.Subscriber('/carla/ego_vehicle/odometry',
                                                     Odometry, self.odometry_received)
 
-        self.twist_publisher = rospy.Publisher(
-            "/carla/ego_vehicle/twist_pid", Twist, queue_size=1)
+        self.local_plan_path = Path()
+        self.local_plan_path.header.frame_id = 'map'
+        self.local_plan_publisher = rospy.Publisher(
+            "/local_plan", Path, queue_size=1)
 
         self.akcermann_publisher = rospy.Publisher(
             "/carla/ego_vehicle/ackermann_cmd", AckermannDrive, queue_size=1)
@@ -82,8 +85,14 @@ class TrajectoryFollower(object):
             target_point = self.local_plan[0]
             _, angle = self.compute_magnitude_angle(target_point, self.current_location, self.current_orientation)
             self.control_cmd.steering_angle = math.radians(np.clip(angle, -60.0, 60.0))
-            self.control_cmd.speed = (self.target_speed - abs(self.control_cmd.steering_angle) * (
-                    self.target_speed - self.min_speed)) / 3.6
+            # self.control_cmd.speed = (self.target_speed - abs(self.control_cmd.steering_angle) * (
+            #         self.target_speed - self.min_speed)) / 3.6
+
+            d, a = self.estimate_curvature(self.current_location, self.current_orientation, self.local_plan)
+            rospy.loginfo('curvature: ' + str(a))
+            fact = np.clip(a/100, 0, 1)
+            self.control_cmd.speed = (self.target_speed - fact * (self.target_speed - self.min_speed)) / 3.6
+
 
     def del_old_points(self):
         max_index = -1
@@ -99,6 +108,8 @@ class TrajectoryFollower(object):
         if max_index >= 0:
             for i in range(max_index + 1):
                 self.local_plan.popleft()
+                self.local_plan_path.poses.pop(0)
+
 
     def fill_point_buffer(self):
         """
@@ -106,9 +117,36 @@ class TrajectoryFollower(object):
         """
         for _ in range(self.buffer_size - len(self.local_plan)):
             if self.global_plan:
-                self.local_plan.append(self.global_plan.popleft())
+                pos = self.global_plan.popleft()
+                self.local_plan.append(pos)
+
+                pose = PoseStamped()
+                pose.header.frame_id = 'map'
+                pose.header.stamp = rospy.Time.now()
+                pose.pose.orientation = Quaternion(0, 0, 0, 0)
+                pose.pose.position = pos
+                self.local_plan_path.poses.append(pose)
             else:
                 break
+
+        self.publish_local_plan()
+
+
+    def publish_local_plan(self):
+        currentPose = PoseStamped()
+        currentPose.header.frame_id = 'map'
+        currentPose.header.stamp = rospy.Time.now()
+        currentPose.pose.orientation = self.current_orientation
+        currentPose.pose.position = self.current_location
+
+        self.local_plan_path.header.frame_id = 'map'
+        self.local_plan_path.header.stamp = rospy.Time.now()
+        self.local_plan_path.poses.insert(0, currentPose)
+        self.local_plan_publisher.publish(self.local_plan_path)
+        self.local_plan_path.poses.pop(0)
+
+
+
 
     def compute_magnitude_angle(self, target_location, current_location, current_orientation):
         """
@@ -124,11 +162,10 @@ class TrajectoryFollower(object):
         dist_target = np.linalg.norm(target_vector)
 
         # vector of the car and absolut angle between vehicle and target point
+        # angle = (a o b) / (|a|*|b|), here: |b| = 1
         forward_vector = np.array([math.cos(math.radians(orientation)), math.sin(math.radians(orientation))])
-        if abs(dist_target) > 0.0:
-            d_angle = math.degrees(math.acos(np.dot(forward_vector, target_vector) / dist_target))
-        else:
-            d_angle = 0.0
+        c = np.clip(np.dot(forward_vector, target_vector) / dist_target, -1.0, 1.0)
+        d_angle = math.degrees(math.acos(c))
 
         # make angle negative or positive
         cross = np.cross(forward_vector, target_vector)
@@ -148,7 +185,41 @@ class TrajectoryFollower(object):
             plt.pause(0.01)
             plt.draw()
 
-        return (dist_target, d_angle)
+        return dist_target, d_angle
+
+    def estimate_curvature(self, current_location, current_orientation, local_plan):
+        angles = []
+        distances = []
+        curvature = 0
+        total_dist = 0
+
+        dist_start, angle_start = self.compute_magnitude_angle(local_plan[0], current_location, current_orientation)
+        angles.append(angle_start)
+        distances.append(dist_start)
+        total_dist += abs(dist_start)
+        curvature += abs(angle_start)
+
+        for i in range(len(local_plan) - 2):
+            vector = np.array([local_plan[i+1].x - local_plan[i].x, local_plan[i+1].y - local_plan[i].y])
+            dist = np.linalg.norm(vector)
+            vector_next = np.array([local_plan[i + 2].x - local_plan[i + 1].x, local_plan[i + 2].y - local_plan[i + 1].y])
+            dist_next = np.linalg.norm(vector_next)
+
+            if dist == 0 or dist_next == 0:
+                continue
+
+            c = np.clip(np.dot(vector, vector_next) / (dist * dist_next), -1.0, 1.0)
+            angle = math.degrees(math.acos(c))
+
+            angles.append(angle)
+            distances.append(dist)
+            total_dist += abs(dist)
+            curvature += abs(angle)
+
+        return total_dist, curvature
+
+
+
 
     def publish_ackermann(self):
         self.akcermann_publisher.publish(self.control_cmd)
