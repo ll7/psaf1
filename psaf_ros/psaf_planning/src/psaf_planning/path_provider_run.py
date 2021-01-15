58c9508a75a42fe8917782975d60e218748a8e63
Sebastian Fischer <sebastian.fischer@embeddedlabs.de>
2021-01-15 18:11:17 +0100

    Fix planner run

--- psaf_ros/psaf_planning/src/psaf_planning/path_provider_run.py
+++ psaf_ros/psaf_planning/src/psaf_planning/path_provider_run.py
@@ -1,19 +1,15 @@
+
 #!/usr/bin/env python
 
 from psaf_planning.global_planner.path_provider_lanelet2 import PathProviderLanelet2
 from psaf_planning.global_planner.path_provider_abstract import PathProviderAbstract
 from psaf_planning.global_planner.path_provider_common_roads import PathProviderCommonRoads
 import rospy
-from psaf_messages.msg import Obstacle
 
 
 def main():
-    rospy.init_node('TEST', anonymous=True)
-    ob = Obstacle(1, 0, 0, -10, 0)
-    pub = rospy.Publisher("/psaf/planning/obstacle", Obstacle, queue_size=10)
-    while pub.get_num_connections() == 0:
-        rospy.sleep(1)
-    pub.publish(ob)
+    provider: PathProviderAbstract = PathProviderCommonRoads(init_rospy=True, enable_debug=False)
+    rospy.spin()
 
 
 if __name__ == "__main__":
