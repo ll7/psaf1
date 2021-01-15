add7de7d9238468418d1317f6e7b85819c8b7732
Markus Keppeler <markus.keppeler@uni-a.de>
2021-01-15 13:42:07 +0100

    Update Local Traffic Planner

--- psaf_ros/psaf_local_planner/src/local_traffic_planner.py
+++ psaf_ros/psaf_local_planner/src/local_traffic_planner.py
@@ -1,19 +1,25 @@
 import rospy
 from std_msgs.msg import UInt8
-from carla_msgs.msg import CarlaEgoVehicleControl, CarlaEgoVehicleStatus
-from psaf_messages.msg import TrafficSignInfo, SpeedSign, StopMark, TrafficLight
+from carla_msgs.msg import CarlaEgoVehicleControl
+from psaf_messages.msg import TrafficSignInfo
 import numpy as np
 
-class Local_Traffic_Planner():
-    # TODO doc
+class Local_Traffic_Planner:
+    """
+        Planner for the traffic rules
+          e.g. the speed limits
+    """
 
-    def __init__(self):
+    def __init__(self,default_speed= 50):
         self.velocity_publisher = rospy.Publisher("psaf_velocity_plan", UInt8, queue_size=1)
 
         rospy.Subscriber("/carla/ego_vehicle/vehicle_control_cmd", CarlaEgoVehicleControl, self.callback_ego_vehicle_cmd)
-        rospy.Subscriber("/carla/ego_vehicle/vehicle_control_cmd", CarlaEgoVehicleControl, self.callback_ego_vehicle_cmd)
+        rospy.Subscriber("/psaf/perception/traffic_signs", TrafficSignInfo, self.callback_traffic_signs)
 
-        self.acceptance_area = np.zeros([3,3])
+        self.acceptance_area = np.zeros([3,3],dtype=float)
+
+        # Current speed in km/h
+        self.current_speed = default_speed
 
     def callback_ego_vehicle_cmd(self,data: CarlaEgoVehicleControl):
 
@@ -21,27 +27,41 @@ class Local_Traffic_Planner():
         x1 = data.steer * (0.3 if data.steer > 0 else 0.6) + 0.5
 
         # Calculate coordinates
-        x2 = min([x1 + width, 1])
-
-        self.acceptance_area = np.ndarray([x1,0.2,x2,0.8])
+        x2 = min([x1 + width, 1.])
+        self.acceptance_area = np.array([x1,0.2,x2,0.8],dtype=float)
 
     def callback_traffic_signs(self,traffic_signs:TrafficSignInfo):
+        """
+        Callback to handle the traffic sign data
+        :param traffic_signs: the message
+        :return: None
+        """
+
         speed_signs = traffic_signs.speedSigns
 
         edited_speed_list = sorted(filter(lambda x: x.distance < 15,speed_signs),key=lambda x:x.distance)
 
         area = self.acceptance_area
-        matches = filter(lambda sign: area[0] <= sign.x.data <= area[2] and area[1] <= sign.y.data <= area[3],edited_speed_list)
+        matches = list(filter(lambda sign: area[0] <= sign.x <= area[2] and area[1] <= sign.y <= area[3],edited_speed_list))
 
-        best = next(iter(matches), None)
+        if len(matches)>0:
+            self.current_speed = matches[0].limit
 
-        msg = UInt8(best.limit)
+    def periodic_planner_input_update(self):
+        """
+        Function that should be run periodically to update the local planner control topics
+        :return: None
+        """
+        msg = UInt8(self.current_speed)
 
-        self.velocity.publish(msg)
+        self.velocity_publisher.publish(msg)
         print(f"I updated the velocity to {msg}")
 
 
+
 if __name__ == '__main__':
+    rospy.init_node("Traffic_Local_planner")
     planner = Local_Traffic_Planner()
+    rospy.spin()
 
 
