fce2be835a04c13e318c9df294189b06e2bcff92
Jonas W <jonas.wilfert@student.uni-augsburg.de>
2021-01-16 15:15:03 +0100

    changed params to different frequencies

--- psaf_ros/psaf_obstacle_layer/src/psaf_obstacle_layer/plugin_obstacle_layer.cpp
+++ psaf_ros/psaf_obstacle_layer/src/psaf_obstacle_layer/plugin_obstacle_layer.cpp
@@ -5,8 +5,12 @@ PLUGINLIB_EXPORT_CLASS(psaf_obstacle_layer::PsafObstacleLayer, costmap_2d::Layer
 
 namespace psaf_obstacle_layer
 {
+    
     void PsafObstacleLayer::updateCosts (costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i, int max_j) {
-        // costmap_2d::ObstacleLayer::resetMaps();
+        static int counter = 0;
+        if (counter++ % 40 == 0) {
+            costmap_2d::ObstacleLayer::reset();
+        }
         costmap_2d::ObstacleLayer::updateCosts(master_grid, min_i, min_j, max_i, max_j);
     }
 } // namespace psaf_local_planner
