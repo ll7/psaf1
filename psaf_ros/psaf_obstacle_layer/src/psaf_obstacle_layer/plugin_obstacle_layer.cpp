cfe613351d0dd31c737e4c35c1eb6a7378443a73
Jonas W <jonas.wilfert@student.uni-augsburg.de>
2021-01-20 12:26:21 +0100

    fixed flickering of costmap

--- psaf_ros/psaf_obstacle_layer/src/psaf_obstacle_layer/plugin_obstacle_layer.cpp
+++ psaf_ros/psaf_obstacle_layer/src/psaf_obstacle_layer/plugin_obstacle_layer.cpp
@@ -8,10 +8,11 @@ namespace psaf_obstacle_layer
     
     void PsafObstacleLayer::updateCosts (costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i, int max_j) {
         static int counter = 0;
-        if (counter++ % 40 == 0) {
+
+        costmap_2d::ObstacleLayer::updateCosts(master_grid, min_i, min_j, max_i, max_j);
+        if (counter++ % 10 == 0) {
             costmap_2d::ObstacleLayer::reset();
         }
-        costmap_2d::ObstacleLayer::updateCosts(master_grid, min_i, min_j, max_i, max_j);
     }
 } // namespace psaf_local_planner
 
