60435c270513ba417702c88ee201fd5c30edc810
Sebastian Fischer <sebastian2.fischer@student.uni-augsburg.de>
2021-01-27 14:22:53 +0100

    add PathSupervisorCommonRoads to launch

--- psaf_ros/psaf_planning/src/psaf_planning/path_provider_run.py
+++ psaf_ros/psaf_planning/src/psaf_planning/path_provider_run.py
@@ -3,11 +3,12 @@
 from psaf_planning.global_planner.path_provider_lanelet2 import PathProviderLanelet2
 from psaf_planning.global_planner.path_provider_abstract import PathProviderAbstract
 from psaf_planning.global_planner.path_provider_common_roads import PathProviderCommonRoads
+from psaf_planning.path_supervisor_common_roads import PathSupervisorCommonRoads
 import rospy
 
 
 def main():
-    provider: PathProviderAbstract = PathProviderCommonRoads(init_rospy=True, enable_debug=False)
+    provider: PathProviderAbstract = PathSupervisorCommonRoads(init_rospy=True, enable_debug=False)
     rospy.spin()
 
 
