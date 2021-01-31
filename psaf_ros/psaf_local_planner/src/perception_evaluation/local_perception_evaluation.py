7736f26b76e94a26f38344a1f99b71f64a43754b
Markus Keppeler <markus.keppeler@uni-a.de>
2021-01-31 21:33:19 +0100

    Filter traffic lights

--- psaf_ros/psaf_local_planner/src/perception_evaluation/local_perception_evaluation.py
+++ psaf_ros/psaf_local_planner/src/perception_evaluation/local_perception_evaluation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+from collections import Counter
 from typing import List
 
 import rospy
 from carla_msgs.msg import CarlaEgoVehicleControl, CarlaEgoVehicleStatus
-from psaf_messages.msg import TrafficSignInfo, TrafficLight, TrafficSituation
+from psaf_messages.msg import TrafficSignInfo, TrafficLight, TrafficSituation, StopLineInfo, StopLine
 import numpy as np
+
+
+def is_in_traffic_light_area(traffic_light: TrafficLight, x1, x2, y1, y2):
+    return x1 <= traffic_light.x <= x2 and y1 <= traffic_light.y <= y2
 
 
 class LocalPerceptionEvaluation:
@@ -13,43 +18,60 @@ class LocalPerceptionEvaluation:
     """
 
     def __init__(self):
-        self.traffic_situation_publisher = rospy.Publisher("/psaf/local_planner/traffic_situation", TrafficSituation, queue_size=1)
+        self.traffic_situation_publisher = rospy.Publisher("/psaf/local_planner/traffic_situation", TrafficSituation,
+                                                           queue_size=1)
 
         rospy.Subscriber("/carla/ego_vehicle/vehicle_control_cmd", CarlaEgoVehicleControl,
                          self.callback_ego_vehicle_cmd)
         rospy.Subscriber("/psaf/perception/traffic_signs", TrafficSignInfo, self.callback_traffic_signs)
+        rospy.Subscriber("/psaf/perception/stop_lines", StopLineInfo, self.callback_stop_lines)
 
         self.publish_timer = rospy.Timer(rospy.Duration(0.1), self.periodic_planner_input_update)
 
-        self.traffic_light_acceptance_areas = np.zeros([2, 4], dtype=float)
+        self.current_traffic_light = None
+        self.distance_to_stop_line = float('inf')
+
+        self.steer = 0.
 
     def callback_ego_vehicle_cmd(self, data: CarlaEgoVehicleControl):
+        self.steer = data.steer
 
-        width = 0.34
-        x1 = data.steer * (0.3 if data.steer > 0 else 0.6) + 0.5
 
-        # Calculate coordinates
-        x2 = min([x1 + width, 1.])
-        self.traffic_light_acceptance_areas[0] = np.array([x1, 0.2, x2, 0.8], dtype=float)
-        self.traffic_light_acceptance_areas[1] = np.array([x1, 0.2, x2, 0.8], dtype=float)
+    def look_for_traffic_light(self,stamp, traffic_lights: List[TrafficLight]):
 
-    def is_in_traffic_light_area(self, traffic_lights: List[TrafficLight]):
-        sorted_traffic_light_list = sorted(traffic_lights, key=lambda x: x.distance)
-        area = self.traffic_light_acceptance_area
-        matches = list(
-            filter(lambda sign: area[0] <= sign.x <= area[2] and area[1] <= sign.y <= area[3],
-                   sorted_traffic_light_list))
+        # Copy the value to work always with he same data
+        steer = self.steer
 
-        if len(matches) > 0:
-            self.next_traffic_light = matches[0]
+        def filter_traffic_light(traffic_light: TrafficLight) -> bool:
+            # only traffic signs that are within 100m and when the state is known
+            if traffic_light.state != traffic_light.STATE_UNKNOWN and traffic_light.distance < 100:
+                # check on right hand side for traffic lights (e.g. in small towns like town02)
+                x1 = steer * (0.3 if steer > 0 else 0.6) + 0.5
+                # Calculate coordinates
+                x2 = min([x1 + 0.34, 1.])
+                first_check = is_in_traffic_light_area(traffic_light, x1, x2, 0.2, 0.8)
+                if first_check:
+                    return True
+                # check for traffic lights that are in front of the car on the other side of the intersection (american style)
+                center_x = steer * (0.25 if steer > 0 else 0.3) + 0.5
+                center_y = 0.375
+
+
+                # scale width and height by distance -> size decrease with distance
+                scaling = 1-(traffic_light.distance-40)/100
+                width = 0.2 * scaling
+                height = 0.25 *scaling
+                return is_in_traffic_light_area(traffic_light,center_x-width/2,center_x+width/2,center_y-height/2,center_y+height/2)
+
+        filtered_traffic_light_list = list(filter(filter_traffic_light,traffic_lights))
+        if len(filtered_traffic_light_list)>0:
+            most_common_state = Counter(map(lambda x:x.state,filtered_traffic_light_list)).most_common(1)
+            closest_with_correct_state = filter(lambda x:x.state== most_common_state,filtered_traffic_light_list)
+
+            self.current_traffic_light = closest_with_correct_state
         else:
-            self.next_traffic_light = None
+            self.current_traffic_light = None
 
-        if len(sorted_traffic_light_list) > 0:
-            self.next_traffic_light = sorted_traffic_light_list[0]
-
-    def look_for_traffic_light(self, traffic_lights: List[TrafficLight]):
-        pass
 
     def callback_traffic_signs(self, traffic_signs: TrafficSignInfo):
         """
@@ -57,15 +79,30 @@ class LocalPerceptionEvaluation:
         :param traffic_signs: the message
         :return: None
         """
-        self.look_for_traffic_light(traffic_signs.trafficLights)
+        self.look_for_traffic_light(traffic_signs.header.stamp,traffic_signs.trafficLights)
+
+    def callback_stop_lines(self, stop_lines: StopLineInfo):
+        """
+           Callback to handle the traffic sign data
+           :param stop_lines: the message
+           :return: None
+           """
+        self.distance_to_stop_line = float('inf')
+        if list(stop_lines.stopLines):
+             self.distance_to_stop_line = min(map(lambda x:x.distance,stop_lines.stopLines))
 
     def periodic_planner_input_update(self, event):
         """
         Function that should be run periodically to update the local planner control topics
         :return: None
         """
-        # TODO
-        self.traffic_situation_publisher.publish(self.next_traffic_light)
+        msg = TrafficSituation()
+        if self.current_traffic_light is not None:
+            msg.trafficLight.append(self.current_traffic_light)
+        msg.distanceToStopLine=self.distance_to_stop_line
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'car'
+        self.traffic_situation_publisher.publish(msg)
 
 
 if __name__ == '__main__':
