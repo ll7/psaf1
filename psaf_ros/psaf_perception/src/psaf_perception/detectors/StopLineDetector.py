866d3d748e61c3adf67ad93c97688b3f8c5a8f9d
Markus Keppeler <markus.keppeler@uni-a.de>
2021-02-10 21:22:21 +0100

    Improve stop line detection

--- psaf_ros/psaf_perception/src/psaf_perception/detectors/StopLineDetector.py
+++ psaf_ros/psaf_perception/src/psaf_perception/detectors/StopLineDetector.py
@@ -1,8 +1,10 @@
+import math
+
 import cv2
 import numpy as np
 import rospy
 
-from psaf_abstraction_layer.sensors.SegmentationCamera import SegmentationCamera,Tag
+from psaf_abstraction_layer.sensors.SegmentationCamera import SegmentationCamera, Tag
 from psaf_perception.detectors.AbstractDetector import AbstractDetector, DetectedObject, Labels
 
 
@@ -10,6 +12,7 @@ class StopLineDetector(AbstractDetector):
     """
     Detector for stop lines
     """
+
     def __init__(self, role_name: str = "ego_vehicle"):
         """
         Init the speed sign detector
@@ -19,26 +22,26 @@ class StopLineDetector(AbstractDetector):
 
         self.logger_name = "TrafficLightDetector"
 
-        self.ratio_range_per_mille = range(5, 150)
+        self.ratio_range_per_mille = range(5, 500)
 
         self.canny_threshold = 100
 
-        self.camera = SegmentationCamera(role_name=role_name,id="front")
+        self.camera = SegmentationCamera(role_name=role_name, id="front")
         self.camera.set_on_image_listener(self.__on_new_image)
 
     @classmethod
-    def __heuristic_calc_distance_by_y(cls, y:float)->float:
+    def __heuristic_calc_distance_by_y(cls, y: float) -> float:
         """
         Calculates the distance in meters by using the y coordinate of the detected object
         :param y: the y coordinate of the stop line center
         :return:
         """
-        a_4= 4341.4
+        a_4 = 4341.4
         a_3 = - 13727
         a_2 = 16300
         a_1 = - 8635.5
         a_0 = 1727.4
-        return a_4*y**4+a_3*y**3+a_2*y**2+a_1*y+a_0
+        return a_4 * y ** 4 + a_3 * y ** 3 + a_2 * y ** 2 + a_1 * y + a_0
 
     def __on_new_image(self, segmentation_image, time):
         (H, W) = segmentation_image.shape[:2]
@@ -46,27 +49,28 @@ class StopLineDetector(AbstractDetector):
         # List oif detected elements
         detected = []
 
-        filter_image = SegmentationCamera.filter_for_tags(segmentation_image,{Tag.RoadLine})
+        filter_image = SegmentationCamera.filter_for_tags(segmentation_image, {Tag.RoadLine})
 
         canny_output = cv2.Canny(filter_image, self.canny_threshold, self.canny_threshold * 2)
         contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        boxes = []
         for i, c in enumerate(contours):
             contours_poly = cv2.approxPolyDP(c, 3, True)
-            x1, y1, w, h = cv2.boundingRect(contours_poly)
-            boxes.append(np.array([x1, y1, w, h]) / np.array([W, H, W, H]))
+            x1, y1, w, h = np.array(cv2.boundingRect(contours_poly)) / np.array([W, H, W, H])
+            rect_coord = cv2.minAreaRect(contours_poly)
+            angle = rect_coord[2]
 
-        for x1, y1, w, h in boxes:
-            if y1>0.3 and x1+w< 0.9:
-                if int(h/w*1000) in self.ratio_range_per_mille and w> 0.3:
-                    detected.append(DetectedObject(x1, y1, w, h, distance=self.__heuristic_calc_distance_by_y(y1), label=Labels.StopLine))
+            if y1 > 0.55 and x1 + w >  0.6 and \
+                 w > 0.3 and -40 < angle < 40 and int(h / w * 1000) in self.ratio_range_per_mille:
+                detected.append(DetectedObject(x1, y1, w, h, distance=self.__heuristic_calc_distance_by_y(y1+h),
+                                               label=Labels.StopLine))
 
-        self.inform_listener(time,detected)
+        self.inform_listener(time, detected)
 
 
 if __name__ == "__main__":
     from psaf_abstraction_layer.sensors.RGBCamera import RGBCamera
+
 
     # Show case code
     def show_image(title, image):
@@ -102,13 +106,13 @@ if __name__ == "__main__":
                 # draw a bounding box rectangle and label on the image
                 color = (255, 0, 0)
                 cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-                text = "{:.1f}m: x:{:.3f}-y:{:.3f}".format(element.distance, element.x,element.y)
+                text = "{:.1f}m: x:{:.3f}-y:{:.3f}".format(element.distance, element.x, element.y)
                 cv2.putText(image, text, (x - 5, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
         show_image("Stop line detection", image)
 
 
-    def on_detected(_,detected_list):
+    def on_detected(_, detected_list):
         global detected_r
         detected_r = detected_list
 
