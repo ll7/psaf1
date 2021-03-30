206d774d133730ccc03d6fd0794b610e1b6c149b
Markus Keppeler <markus.keppeler@uni-a.de>
2021-03-30 18:20:47 +0200

    local plan evaluation factor lowered

--- psaf_ros/psaf_local_planner/src/psaf_local_planner/Main.cpp
+++ psaf_ros/psaf_local_planner/src/psaf_local_planner/Main.cpp
@@ -11,7 +11,7 @@ namespace psaf_local_planner
                                             lookahead_factor(0.25), lookahead_factor_exp(1.4), lookahead_factor_const_additive(1.5), 
                                             goal_reached(false), estimate_curvature_distance(50), check_collision_max_distance(40),
                                             slow_car_ahead_counter(0), slow_car_ahead_published(false), obstacle_msg_id_counter(0), 
-                                            duration_factor(2.0), distance_factor(2.0), respect_traffic_rules(true), max_points_smoothing(10), 
+                                            duration_factor(1.5), distance_factor(1.5), respect_traffic_rules(true), max_points_smoothing(10),
                                             lane_change_direction(0), lane_change_direction_calculated(false), target_velocity(15), min_velocity(5)
     {
         std::cout << "Hi";
